8863d6a214f31b2df144d02eb4abf4c6ae5ce1db
Michael Abebe <michaelabebe@michaels-mbp.myfiosgateway.com>
2022-03-08 23:29:43 -0500

    LeetCode Python train

---
+++ python-relearning.py
@@ -0,0 +1,11 @@
+import numpy as np
+a = np.array((3, 4))
+b = np.linspace(0, 11, num=12)
+print(np.shape(a))
+print(a.shape)
+print(b)
+print(b.shape)
+c = np.arange(6, -1, -1)
+print(c)
+msg = "Hello World"
+print(msg)
